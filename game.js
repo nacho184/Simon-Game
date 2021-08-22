5aa5153efebe8b0913a2436f7a2ce662569d37b6
nacho184 <87767671+nacho184@users.noreply.github.com>
2021-08-22 08:24:50 +0200

    Update game.js

--- game.js
+++ game.js
@@ -59,7 +59,7 @@ function checkAnswer(currentLevel) {
   setTimeout(function () {
     $("body").removeClass("game-over");
   }, 200);
-  $("#level-title").text("Game Over, Press Any Key to Restart");
+  $("#level-title").text("Game Over, Press Any Key or reload the page");
 
   startOver();
   
